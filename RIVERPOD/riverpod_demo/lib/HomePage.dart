5af97f039e42a6da6ec4bcdc39a28e132f4524dd
Tento <regartento@gmail.com>
2026-02-10 14:29:15 +0700

    add state provider

--- RIVERPOD/riverpod_demo/lib/HomePage.dart
+++ RIVERPOD/riverpod_demo/lib/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final greetingProvider = Provider<String>((ref) => "Hello Riverpod");
+final counterProvider = StateProvider<int>((ref) => 0);
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -9,9 +10,25 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final greeting = ref.watch(greetingProvider);
+    final counter = ref.watch(counterProvider);
 
     return Scaffold(
-      body: Center(child: Text(greeting, style: TextStyle(fontSize: 30))),
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Column(
+          children: [
+            Text(greeting),
+            SizedBox(height: 10),
+            Text("Counter:$counter"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
